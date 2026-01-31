3512d93ff91997eff7a0c9f6b853d6772a492453
Debby-arch <opiyorosedebrah@gmail.com>
2026-01-31 07:19:30 +0300

    hero text

--- src/Components/Navbar/Hero/Hero.jsx
+++ src/Components/Navbar/Hero/Hero.jsx
@@ -8,7 +8,8 @@ const Hero = () => {
     <div className='hero container'>
     <div className='hero-text'>
         <h1>Evolve Better. Work Smarter.</h1>
-        <p>Evolence helps teams evolve how work gets done-effectively and intelligently.</p>
+        <p>I help schools, SMEs, and founder-led teams reduce operational chaos and inefficiency to work smarter, scale sustainably, and make better decisions by digitising processes, automating workflows, and building clear, practical systems powered by data and modern tools.
+</p>
         <button className='btn'>Explore More <img src={dark_arrow} alt=""/></button>
     </div>
       
