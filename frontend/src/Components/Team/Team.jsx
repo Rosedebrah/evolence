3bacd1c7d6b848957bcaa1cc95d8e252fd8a5b2c
Debby-arch <opiyorosedebrah@gmail.com>
2026-06-11 22:41:51 +0300

    Add cpoPhoto

--- frontend/src/Components/Team/Team.jsx
+++ frontend/src/Components/Team/Team.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './Team.css'
 import { FaLinkedinIn } from 'react-icons/fa'
 import { HiOutlineMail } from 'react-icons/hi'
-import cpoPhoto from '../../assets/cpo.jpg'
+import cpoPhoto from '../../assets/cpo.JPG'
 import ctoPhoto from '../../assets/cto.png'
 
 const founders = [
